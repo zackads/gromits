16ee02e2ada125036e4e0fb2e0ad5f5c7ad4d107
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-03-20 14:02:08 +0000

    Buildings displaying on map (albeit in wrong place)

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,26 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const SERVICE = 'http://localhost:8875/buildings';
-// process.env.BUILDINGS_HOSTNAME + ':' + process.env.BUILDINGS_PORT;
 
 function App() {
-  axios
-    .get(SERVICE)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const [buildings, setBuildings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(SERVICE)
+      .then((response) => {
+        setBuildings(response.data.points);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   return (
     <div id='mapid'>
+      {isLoading && <p>Loading buildings...</p>}
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+        {buildings.slice(0, 10).map((building, index) => {
+          return (
+            <Marker
+              key={index}
+              position={building.geometry.coordinates.reverse()}
+            >
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Marker>
+          );
+        })}
         <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
