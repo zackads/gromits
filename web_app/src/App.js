42fc20408c1492256dc0cbc5ee14afdfa99f1dc3
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-03-20 20:25:58 +0000

    Displays buildings on map

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,55 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Map from './components/Map';
 
 const CENTRE = '@51.4663,-2.6012';
 const SERVICE = 'http://localhost:8875/buildings/' + CENTRE;
 
-function App() {
-  const [buildings, setBuildings] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+class App extends Component {
+  state = {
+    buildings: [],
+  };
 
-  useEffect(() => {
-    axios
-      .get(SERVICE)
-      .then((response) => {
-        setBuildings(response.data.points);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  async componentDidMount() {
+    const res = await axios.get(SERVICE);
+    const buildings = res.data.points;
+    this.setState({ buildings: buildings });
+  }
 
-  return (
-    <div id='mapid'>
-      {isLoading && <p>Loading buildings...</p>}
-      <MapContainer
-        center={[51.4663, -2.6012]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        {buildings.map((building, index) => {
-          return (
-            <Marker key={index} position={building.geometry.coordinates}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          );
-        })}
-        <Marker position={[51.46283, -2.59553]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+  render() {
+    return <Map buildings={this.state.buildings} />;
+  }
 }
 
 export default App;
---
+++ web_app/src/components/Map.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+export default class Map extends Component {
+  state = {
+    center: [51.4663, -2.6012],
+    zoom: 13,
+  };
+  render() {
+    return this.props.buildings ? (
+      <MapContainer
+        center={this.state.center}
+        zoom={this.state.zoom}
+        style={{ width: '100%', height: '900px' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {this.props.buildings.map((building, i) => {
+          const [lat, long] = building.geometry.coordinates.reverse();
+          return <Marker position={[lat, long]} key={i}></Marker>;
+        })}
+        <Marker position={[51.4573, -2.59882]}></Marker>
+      </MapContainer>
+    ) : (
+      'Data is loading'
+    );
+  }
+}
