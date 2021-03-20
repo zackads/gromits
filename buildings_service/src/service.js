1c48d833a6ca6c4739a0d3ceb72bcbc23dd0ac3b
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-03-20 12:58:01 +0000

    Extract environment variables to app-wide .env

--- buildings_service/src/service.js
+++ buildings_service/src/service.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 import { Building } from './models/building';
 
 const app = express();
-const PORT = 8875;
+const DB_HOSTNAME = process.env.DB_HOSTNAME;
+const SERVICE_PORT = 8875;
 
-mongoose.connect('mongodb://localhost', {
+mongoose.connect(`mongodb://${DB_HOSTNAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -15,8 +16,8 @@ console.log('hello world!');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
-app.listen(PORT, () => {
-  console.log(`Listening at port ${PORT}...`);
+app.listen(SERVICE_PORT, () => {
+  console.log(`Listening at port ${SERVICE_PORT}...`);
 });
 
 app.get('/buildings', (req, res) => {
