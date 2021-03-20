0b26aa1e055b689773c8e290a9d141b006a6907e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-03-20 10:27:49 +0000

    Working buildings_service Dockerfile

--- buildings_service/dist/index.js
+++ buildings_service/dist/index.js
@@ -2,9 +2,29 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _building = require("./models/building");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
-app.listen(80, function () {
-  console.log('Listening at port 80...');
+var port = 80;
+
+_mongoose["default"].connect("mongodb://localhost", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+var db = _mongoose["default"].connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+app.listen(port, function () {
+  console.log("Listening at port 80...");
+});
+app.get("/buildings", function (req, res) {
+  return _building.Building.find(function (err, buildings) {
+    return res.send({
+      points: buildings
+    });
+  });
 });
