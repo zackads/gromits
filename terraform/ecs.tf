1c61ad5192bd70c9d479f2a4046aa92f5fc6a601
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-02 15:10:03 +0100

    Restrict access only on port 80

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -33,7 +33,7 @@ resource "aws_ecs_service" "webapp" {
 
   network_configuration {
     subnets          = aws_subnet.public.*.id
-    security_groups = [aws_security_group.allow_all.id]
+    security_groups = [aws_security_group.allow_http.id]
     assign_public_ip = true
   }
 }
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,12 +1,12 @@
-resource "aws_security_group" "allow_all" {
+resource "aws_security_group" "allow_http" {
     name = "allow_http_sg"
     description = "Allow inbound HTTP connections"
     vpc_id = aws_vpc.main.id
 
     ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
