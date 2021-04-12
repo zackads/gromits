d257038b4f1c909479d16339d7acb1898ec6d7d2
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-12 19:58:58 +0100

    Move Terraform state to remote S3/DynamoDB

---
+++ web_app/terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket = "zackads-terraform-state"
+        dynamodb_table = "zackads-terraform-state"
+        encrypt = true
+        key = "gromits/web_app/state.tfstate"
+        region = "eu-west-2"
+    }
+}
