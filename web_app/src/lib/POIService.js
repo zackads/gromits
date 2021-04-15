e5d5b2737ca608fe8b330663e161e8b606786391
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-15 07:10:05 +0100

    Remove app from serverless.yml to fix deploy

--- web_app/src/lib/POIService.js
+++ web_app/src/lib/POIService.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
 
-const POI_SERVICE_URI = process.env.REACT_APP_POI_SERVICE_URI;
+const POI_SERVICE_URI =
+  'https://l5m7m3ixeb.execute-api.eu-west-2.amazonaws.com/dev/buildings';
 
-export const POIService = {
+export const POIGateway = {
   getBuildingsNear: async (point) => {
     try {
       const { data: response } = await axios.get(
-        'https://' + POI_SERVICE_URI + '/' + locationParam(point)
+        POI_SERVICE_URI + '/' + locationParam(point)
       );
       return response;
     } catch (error) {
