94f333219f1a2c9c2bf68e67b4a421ce2694be2b
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-17 12:52:46 +0100

    Test name and grade appear on Marker

--- web_app/src/components/Map.test.tsx
+++ web_app/src/components/Map.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { render, waitFor, screen } from "@testing-library/react";
 import { Map } from "./Map";
 import { LatLng } from "../lib/entities/LatLng";
@@ -6,42 +7,54 @@ import {
   IPointOfInterest,
   Shapes,
 } from "../lib/entities/IPointOfInterest";
+import { IPoiGateway } from "../lib/gateways/IPoiGateway";
 
 describe("Map", () => {
-  const arbitrary_point: LatLng = [51.4846117, -0.0072328];
+  const mapCentre: LatLng = [51.4846117, -0.0072328];
+
+  const building: IPointOfInterest = {
+    id: "1",
+    properties: {
+      name: "Nakatomi Plaza",
+      listEntry: 42,
+      location: "2121 Avenue of the Stars, Los Angeles",
+      grade: BuildingGrades.two,
+      hyperlink: "https://en.wikipedia.org/wiki/Die_Hard_(film_series)",
+    },
+    geometry: { type: Shapes.Point, coordinates: [51.4846117, -0.0072328] },
+  };
+
+  let mockPoiGateway: IPoiGateway;
+
+  beforeEach(() => {
+    mockPoiGateway = {
+      fetchNear: jest.fn().mockResolvedValue([building]),
+    };
+  });
 
   it("queries the injected poiGateway for POI markers to display", async () => {
-    const mockPoiGateway = {
-      fetchNear: jest.fn().mockResolvedValue([]),
-    };
-
-    render(<Map centre={arbitrary_point} poiGateway={mockPoiGateway} />);
+    render(<Map centre={mapCentre} poiGateway={mockPoiGateway} />);
 
     await waitFor(() => {
-      expect(mockPoiGateway.fetchNear).toHaveBeenCalledWith(arbitrary_point);
+      expect(mockPoiGateway.fetchNear).toHaveBeenCalledWith(mapCentre);
     });
   });
 
-  it("displays markers", async () => {
-    const building: IPointOfInterest = {
-      id: "1",
-      properties: {
-        name: "Nakatomi Plaza",
-        listEntry: 42,
-        location: "2121 Avenue of the Stars, Los Angeles",
-        grade: BuildingGrades.two,
-        hyperlink: "https://en.wikipedia.org/wiki/Die_Hard_(film_series)",
-      },
-      geometry: { type: Shapes.Point, coordinates: arbitrary_point },
-    };
-    const mockPoiGateway = {
-      fetchNear: jest.fn().mockResolvedValue([building]),
-    };
-
-    render(<Map centre={arbitrary_point} poiGateway={mockPoiGateway} />);
+  it("user can see points of interest", async () => {
+    render(<Map centre={mapCentre} poiGateway={mockPoiGateway} />);
 
     await waitFor(() => {
       expect(screen.getAllByAltText("Listed building")).toHaveLength(1);
+    });
+  });
+
+  it("user can click on a point of interest to see its relevant properties", async () => {
+    render(<Map centre={mapCentre} poiGateway={mockPoiGateway} />);
+
+    await waitFor(() => {
+      screen.getByAltText("Listed building").click();
+      expect(screen.getByText(building.properties.name, { exact: false }));
+      expect(screen.getByText(building.properties.grade, { exact: false }));
     });
   });
 });
