a4b25c09bbc6f47e2f1fb6b7d83b0d3ef53067fe
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-25 11:10:40 +0100

    Fix linting

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -1,26 +1,24 @@
-import React, { FunctionComponent, Component } from "react";
+import React, { FunctionComponent } from "react";
 import { IMarkerOptions, Map } from "./components/Map";
 import { BuildingsGateway } from "./lib/gateways/BuildingsGateway";
 import { LocateControl } from "./components/controls/LocateControl";
 import { SearchControl } from "./components/controls/SearchControl";
 
 const App: FunctionComponent = (): JSX.Element => {
-  {
-    const markerOptions: IMarkerOptions = {
-      maxCount: 1000,
-      tooManyMessage: "Uh-oh - too many buildings!  Try zooming in."
-    };
+  const markerOptions: IMarkerOptions = {
+    maxCount: 1000,
+    tooManyMessage: "Uh-oh - too many buildings!  Try zooming in.",
+  };
 
-    return (
-        <Map
-            poiGateway={new BuildingsGateway()}
-            centre={[51.454095, -2.595995]}
-            markerOptions={markerOptions}
-        >
-          <LocateControl />, <SearchControl />
-        </Map>
-    );
-  }
-}
+  return (
+    <Map
+      poiGateway={new BuildingsGateway()}
+      centre={[51.454095, -2.595995]}
+      markerOptions={markerOptions}
+    >
+      <LocateControl />, <SearchControl />
+    </Map>
+  );
+};
 
 export default App;
