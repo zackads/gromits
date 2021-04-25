a0e12071ad65ad674ead73dc4812faf70fbc6191
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-25 10:53:34 +0100

    Make App a function component

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -4,21 +4,21 @@ import { BuildingsGateway } from "./lib/gateways/BuildingsGateway";
 import { LocateControl } from "./components/controls/LocateControl";
 import { SearchControl } from "./components/controls/SearchControl";
 
-class App extends Component {
-  render() {
+const App: FunctionComponent = (): JSX.Element => {
+  {
     const markerOptions: IMarkerOptions = {
       maxCount: 1000,
       tooManyMessage: "Uh-oh - too many buildings!  Try zooming in."
     };
 
     return (
-      <Map
-        poiGateway={new BuildingsGateway()}
-        centre={[51.454095, -2.595995]}
-        markerOptions={markerOptions}
-      >
-        <LocateControl />, <SearchControl />
-      </Map>
+        <Map
+            poiGateway={new BuildingsGateway()}
+            centre={[51.454095, -2.595995]}
+            markerOptions={markerOptions}
+        >
+          <LocateControl />, <SearchControl />
+        </Map>
     );
   }
 }
