71ef74ec136122a4c4b5e938d827863df5eb2f05
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-22 09:46:31 +0100

    Set Actions workflow to push to new bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "zackads-terraform-state"
-        dynamodb_table = "zackads-terraform-state"
-        encrypt = true
-        key = "gromits/web_app/state.tfstate"
-        region = "eu-west-2"
-    }
+  backend "s3" {
+    bucket = "zackads-terraform-state"
+    dynamodb_table = "zackads-terraform-state"
+    encrypt = true
+    key = "gromits/web_app/state.tfstate"
+    region = "eu-west-2"
+  }
 }
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -39,7 +39,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations = ["GB"]
+      locations = [
+        "GB"]
     }
   }
 
