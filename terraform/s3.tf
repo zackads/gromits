c46fbfe091abb537dee14b954425297dada946b4
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-22 09:43:09 +0100

    Fix s3 policy indentation

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,37 +1,37 @@
 resource "aws_s3_bucket" "prod" {
   bucket = var.domain
-  acl = "public-read"
+  acl    = "public-read"
 
   force_destroy = true
 
   cors_rule {
     allowed_headers = [
-      "*"]
+    "*"]
     allowed_methods = [
       "PUT",
-      "POST"]
+    "POST"]
     allowed_origins = [
-      "*"]
+    "*"]
     expose_headers = [
-      "ETag"]
+    "ETag"]
     max_age_seconds = 3000
   }
 
   policy = <<EOF
-    {
-        "Version": "2008-10-17",
-        "Statement": [
-        {
-            "Sid": "PublicReadForGetBucketObjects",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "*"
-             },
-             "Action": "s3:GetObject",
-             "Resource": "arn:aws:s3:::${var.domain}/*"
-        }]
-    }
-  EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+  {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+          "AWS": "*"
+       },
+       "Action": "s3:GetObject",
+       "Resource": "arn:aws:s3:::${var.domain}/*"
+  }]
+}
+EOF
 
   website {
     index_document = "index.html"
